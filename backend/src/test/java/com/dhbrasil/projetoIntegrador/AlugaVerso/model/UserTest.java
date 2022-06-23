896c5d5faee2675fa173b3dcca7a91ee6e2a904d
Jehan Lucas Vieira <lucasarago.df@gmail.com>
2022-06-23 17:44:11 -0300

    teste 02

---
+++ backend/src/test/java/com/dhbrasil/projetoIntegrador/AlugaVerso/model/UserTest.java
@@ -0,0 +1,17 @@
+package com.dhbrasil.projetoIntegrador.AlugaVerso.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+	
+	
+	@Test
+	public void UserInstance() {
+		User obj = new User();
+
+	}	
+	
+
+}
